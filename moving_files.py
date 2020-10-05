221374b065c67cc6f494cfbe9d26dc487960c432
Stepan <34761679+makeyousure@users.noreply.github.com>
2020-10-05 14:12:42 +0300

    Initial commit

---
+++ moving_files.py
@@ -0,0 +1,32 @@
+import os
+from os.path import isfile, join
+
+catalog_dir = "D:/Разное/Call/"
+only_files = os.listdir(catalog_dir)
+
+
+def create_new_folder(file_of_record):
+    checked_file_date = file_of_record.split(' ')
+    split_date_to_list = checked_file_date[-2].split('-')
+    get_year = split_date_to_list[-1]
+    get_month = split_date_to_list[-2]
+    path_for_move = '{}{}/{}/'.format(catalog_dir, get_year, get_month) + file_of_record
+
+    if os.path.isdir(catalog_dir+get_year):
+        if os.path.isdir('{}{}/{}'.format(catalog_dir, get_year, get_month)):
+            os.replace(catalog_dir + file_of_record, path_for_move)
+        else:
+            os.mkdir('{}{}/{}'.format(catalog_dir, get_year, get_month))
+            os.replace(catalog_dir + file_of_record, path_for_move)
+    else:
+        os.mkdir('{}{}'.format(catalog_dir, get_year))
+        os.mkdir('{}{}/{}'.format(catalog_dir, get_year, get_month))
+        os.replace(catalog_dir + file_of_record, path_for_move)
+
+
+for file in only_files:
+    if isfile(join(catalog_dir, file)):
+        if os.stat(catalog_dir+file).st_size <= 45000:
+            os.remove(catalog_dir+file)
+        else:
+            create_new_folder(file)
---
+++ rename_file.py
@@ -0,0 +1,50 @@
+import os
+from os.path import isfile, join
+
+catalog_dir = "D:/Разное/Call/"
+bad_chars = ['§']
+only_files = os.listdir(catalog_dir)
+
+
+def change_date_format(bad_format_of_date):
+    """Изменяет формат даты в названии звонка"""
+    split_bad_date_format = bad_format_of_date.split('-')
+    return '{}-{}-{}'.format(split_bad_date_format[2], split_bad_date_format[1], split_bad_date_format[0])
+
+
+def return_name_from_contactlist(list_of_name_parts):
+    """Преобразует список из частей имени в строку"""
+    name_to_string = ''
+    for part in list_of_name_parts:
+        if part == list_of_name_parts[-1]:
+            name_to_string += '{}'.format(part)
+        else:
+            name_to_string += '{} '.format(part)
+    return name_to_string
+
+
+def rename_file_with_good_name(old_name, caller_name, date_of_call, time_of_call):
+    os.rename(os.path.join(catalog_dir + old_name),
+              os.path.join(catalog_dir + '{} {} {}'.format(caller_name, date_of_call, time_of_call)))
+
+
+def final_name_for_file(name_of_file):
+    """На вход принимает файл.
+        Разбивает имя на части. Если длина списка элементов имени равна 3-м и не начинается с '§',
+        то меняет имя в нужный формат.
+        Иначе заменяет нукдобное имя на нужное"""
+    list_of_elements = name_of_file.split(' ')
+    name = list_of_elements[:-2]
+    date_of_call = change_date_format(list_of_elements[-2])
+    time_of_call = list_of_elements[-1]
+    if name[0][0] in bad_chars:
+        name = 'From Contacts'
+        rename_file_with_good_name(name_of_file, name, date_of_call, time_of_call)
+    else:
+        name = return_name_from_contactlist(name)
+        rename_file_with_good_name(name_of_file, name, date_of_call, time_of_call)
+
+
+for file in only_files:
+    if isfile(join(catalog_dir, file)):
+        final_name_for_file(file)
